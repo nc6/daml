nc6/daml

commit 7cc5c3eb71d1a7488644d702876a3f48d63be002

Update ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/participant/util/LfEngineToApi.scala
Co-Authored-By: Stephen Compall <scompall@nocandysw.com>

ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/participant/util/LfEngineToApi.scala
@@ -211,6 +211,9 @@ object LfEngineToApi {
       lf: KeyWithMaintainers[Lf.VersionedValue[Lf.AbsoluteContractId]]): Either[String, ApiValue] =
     lfVersionedValueToApiValue(verbose, lf.key)
 
+  @throws[RuntimeException]
+  private[digitalasset] def assertOrRuntimeEx[A](failureContext: String, ea: Either[String, A]): A =
+    ea fold (e => throw new RuntimeException(s"Unexpected error when $failureContext: $e"), identity)
   /** This traversal fails the identity law so is unsuitable for [[scalaz.Traverse]].
     * It is, nevertheless, what is meant sometimes.
     */

commit 7f201b4538ddaff248fa9dd727b68b7915e87547

Use newly introduce assertOrRuntimeEx method
Applies the suggestion introduced by https://github.com/digital-asset/daml/pull/1586/commits/7cc5c3eb71d1a7488644d702876a3f48d63be002

ledger/api-server-damlonx/src/main/scala/com/daml/ledger/api/server/damlonx/services/DamlOnXActiveContractsService.scala
@@ -95,21 +95,16 @@ class DamlOnXActiveContractsService private (
         a.contractId.coid,
         Some(LfEngineToApi.toApiIdentifier(a.templateId)),
         a.contractKey.map(
-          LfEngineToApi
-            .lfContractKeyToApiValue(verbose, _)
-            .fold(
-              err =>
-                throw new RuntimeException(
-                  s"Unexpected error when converting stored contract: $err"),
-              identity)),
+          ck =>
+            LfEngineToApi.assertOrRuntimeEx(
+              "converting stored contract",
+              LfEngineToApi
+                .lfContractKeyToApiValue(verbose, ck))),
         Some(
-          LfEngineToApi
-            .lfValueToApiRecord(verbose = verbose, a.argument.value)
-            .fold(
-              err =>
-                throw new RuntimeException(
-                  s"Unexpected error when converting stored contract: $err"),
-              identity)),
+          LfEngineToApi.assertOrRuntimeEx(
+            "converting stored contract",
+            LfEngineToApi
+              .lfValueToApiRecord(verbose = verbose, a.argument.value))),
         a.stakeholders.toSeq
       ))
   }

ledger/api-server-damlonx/src/main/scala/com/daml/ledger/api/server/damlonx/services/backport/TransactionConversion.scala
@@ -94,19 +94,14 @@ trait TransactionConversion {
       create.contractId.coid,
       Some(LfEngineToApi.toApiIdentifier(create.templateId)),
       create.contractKey.map(
-        LfEngineToApi
-          .lfContractKeyToApiValue(verbose, _)
-          .fold(
-            err =>
-              throw new RuntimeException(s"Unexpected error when converting stored contract: $err"),
-            identity)),
+        ck =>
+          LfEngineToApi.assertOrRuntimeEx(
+            "converting stored contract",
+            LfEngineToApi.lfContractKeyToApiValue(verbose, ck))),
       Some(
-        LfEngineToApi
-          .lfValueToApiRecord(verbose, create.argument.value)
-          .fold(
-            err =>
-              throw new RuntimeException(s"Unexpected error when converting stored contract: $err"),
-            identity)),
+        LfEngineToApi.assertOrRuntimeEx(
+          "converting stored contract",
+          LfEngineToApi.lfValueToApiRecord(verbose, create.argument.value))),
       if (includeParentWitnesses) convert(create.witnesses)
       else convert(create.stakeholders),
       Some(create.agreementText)

ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/participant/util/LfEngineToApi.scala
@@ -38,6 +38,7 @@ import scala.collection.IterableLike
 import scala.collection.generic.CanBuildFrom
 
 object LfEngineToApi {
+
   private[this] type LfValue[+Cid] = Lf[Cid]
 
   def toApiIdentifier(identifier: Identifier) = {
@@ -212,8 +213,9 @@ object LfEngineToApi {
     lfVersionedValueToApiValue(verbose, lf.key)
 
   @throws[RuntimeException]
-  private[digitalasset] def assertOrRuntimeEx[A](failureContext: String, ea: Either[String, A]): A =
-    ea fold (e => throw new RuntimeException(s"Unexpected error when $failureContext: $e"), identity)
+  def assertOrRuntimeEx[A](failureContext: String, ea: Either[String, A]): A =
+    ea.fold(e => throw new RuntimeException(s"Unexpected error when $failureContext: $e"), identity)
+
   /** This traversal fails the identity law so is unsuitable for [[scalaz.Traverse]].
     * It is, nevertheless, what is meant sometimes.
     */

ledger/ledger-api-common/src/main/scala/com/digitalasset/platform/server/api/services/grpc/GrpcTransactionService.scala
@@ -214,11 +214,11 @@ class GrpcTransactionService(
       contractId.unwrap,
       Some(LfEngineToApi.toApiIdentifier(templateId)),
       contractKey.map(
-        LfEngineToApi
-          .lfValueToApiValue(verbose, _)
-          .fold(
-            _ => throw new RuntimeException("Unexpected error when translating the contract key"),
-            identity)),
+        ck =>
+          LfEngineToApi.assertOrRuntimeEx(
+            "translating the contract key",
+            LfEngineToApi
+              .lfValueToApiValue(verbose, ck))),
       Some(
         LfEngineToApi
           .lfValueToApiRecord(verbose, createArguments)

ledger/sandbox/src/main/scala/com/digitalasset/platform/sandbox/services/ApiActiveContractsService.scala
@@ -67,24 +67,18 @@ class ApiActiveContractsService private (
                             create.contractId.coid,
                             Some(LfEngineToApi.toApiIdentifier(create.templateId)),
                             create.contractKey.map(
-                              LfEngineToApi
-                                .lfVersionedValueToApiValue(verbose = request.verbose, _)
-                                .fold(
-                                  err =>
-                                    throw new RuntimeException(
-                                      s"Unexpected error when converting stored contract: $err"),
-                                  identity)),
+                              ck =>
+                                LfEngineToApi.assertOrRuntimeEx(
+                                  "converting stored contract",
+                                  LfEngineToApi
+                                    .lfVersionedValueToApiValue(verbose = request.verbose, ck))),
                             Some(
-                              LfEngineToApi
-                                .lfValueToApiRecord(
-                                  verbose = request.verbose,
-                                  create.argument.value)
-                                .fold(
-                                  err =>
-                                    sys.error(
-                                      s"Unexpected error when converting stored contract: $err"),
-                                  identity)
-                            ),
+                              LfEngineToApi.assertOrRuntimeEx(
+                                "converting stored contract",
+                                LfEngineToApi
+                                  .lfValueToApiRecord(
+                                    verbose = request.verbose,
+                                    create.argument.value))),
                             create.stakeholders.toSeq
                           )
                         )